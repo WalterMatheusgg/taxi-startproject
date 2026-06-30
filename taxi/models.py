611331bd78982552fc55ef30c28d7a4abbe31959
Walter_504 <157768009+WalterMatheusgg@users.noreply.github.com>
2026-06-30 12:45:18 -0300

    Refactor models for Manufacturer and Driver classes

--- taxi/models.py
+++ taxi/models.py
@@ -1,40 +1,71 @@
-from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+
 from django.db import models
 
 
+
+
+
 class Manufacturer(models.Model):
-    name = models.CharField(
-        max_length=255,
-        unique=True
-    )
+
+    name = models.CharField(max_length=255, unique=True)
+
     country = models.CharField(max_length=255)
 
-    def __str__(self) -> str:
+
+
+    def __str__(self):
+
         return self.name
 
 
-class Driver(AbstractUser):
-    license_number = models.CharField(
-        max_length=255,
-        unique=True
-    )
 
-    def __str__(self) -> str:
+
+
+class Driver(AbstractUser):
+
+    license_number = models.CharField(max_length=255, unique=True)
+
+
+
+    REQUIRED_FIELDS = ["license_number"]
+
+
+
+    def __str__(self):
+
         return self.username
 
 
+
+
+
 class Car(models.Model):
+
     model = models.CharField(max_length=255)
+
     manufacturer = models.ForeignKey(
+
         Manufacturer,
+
         on_delete=models.CASCADE,
+
         related_name="cars"
-    )
-    drivers = models.ManyToManyField(
-        settings.AUTH_USER_MODEL,
-        related_name="cars"
+
     )
 
-    def __str__(self) -> str:
+    drivers = models.ManyToManyField(
+
+        Driver,
+
+        related_name="cars",
+
+        blank=True
+
+    )
+
+
+
+    def __str__(self):
+
         return self.model
